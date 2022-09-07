41a4614917edf6ba04217b027a5b397d34d69474
BerkiManman <berkayguler7@hotmail.com>
2022-09-07 17:02:09 +0300

    basic routing

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 require('dotenv').config();
-//const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 const methodOverride = require('method-override');
+const pageRoute = require('./routes/pageRoute');
 const ejs = require('ejs');
 
 const app = express();
@@ -18,37 +19,60 @@ app.use(
         methods: ['POST', 'GET'],
     })
 );
+app.use('/', pageRoute);
 
-app.get('/', (req, res) => {
-    res.status(200).render('index', {
-        page_name: 'index'
+// app.get('/', (req, res) => {
+//     res.status(200).render('index', {
+//         page_name: 'index'
+//     });
+// })
+
+// app.get('/about', (req, res) => {
+//     res.status(200).render('about', {
+//         page_name: 'about'
+//     });
+// })
+
+// app.get('/courses', (req, res) => {
+//     res.status(200).render('courses', {
+//         page_name: 'courses'
+//     });
+// })
+
+// app.get('/dashboard', (req, res) => {
+//     res.status(200).render('dashboard', {
+//         page_name: 'dashboard'
+//     });
+// })
+
+// app.get('/contact', (req, res) => {
+//     res.status(200).render('contact', {
+//         page_name: 'contact'
+//     });
+// })
+
+// app.get('/login', (req, res) => {
+//     res.status(200).render('login', {
+//         page_name: 'login'
+//     });
+// })
+
+// app.get('/signup', (req, res) => {
+//     res.status(200).render('register', {
+//         page_name: 'signup'
+//     });
+// })
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => {
+    console.log('DBCONN!');
+    app.listen(process.env.PORT, () => {
+        console.log(`Server is live on port ${process.env.PORT}`);
     });
 })
-
-app.get('/about', (req, res) => {
-    res.status(200).render('about', {
-        page_name: 'about'
-    });
-})
-
-app.get('/courses', (req, res) => {
-    res.status(200).render('courses', {
-        page_name: 'courses'
-    });
-})
-
-app.get('/dashboard', (req, res) => {
-    res.status(200).render('dashboard', {
-        page_name: 'dashboard'
-    });
-})
-
-app.get('/contact', (req, res) => {
-    res.status(200).render('contact', {
-        page_name: 'contact'
-    });
-})
-
-app.listen(process.env.PORT, () => {
-    console.log(`Server is listening on port: ${process.env.PORT}`);
-})
+.catch((err) => {
+    console.log(err);
+});
