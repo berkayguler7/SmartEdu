292493484b09d22dbf0c5d09fd9c59b83d2f57fb
BerkiManman <berkayguler7@hotmail.com>
2022-09-07 17:01:32 +0300

    basic routing

---
+++ controllers/pageController.js
@@ -0,0 +1,29 @@
+exports.getIndexPage = async (req, res) => {
+  res.status(200).render("index", {
+    page_name: "index",
+  });
+};
+
+exports.getAboutPage = (req, res) => {
+  res.status(200).render("about", {
+    page_name: "about",
+  });
+};
+
+exports.getRegisterPage = (req, res) => {
+  res.status(200).render("register", {
+    page_name: "register",
+  });
+};
+
+exports.getLoginPage = (req, res) => {
+  res.status(200).render("login", {
+    page_name: "login",
+  });
+};
+
+exports.getContactPage = (req, res) => {
+  res.status(200).render("contact", {
+    page_name: "contact",
+  });
+};
