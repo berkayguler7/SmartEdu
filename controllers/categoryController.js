1d60c392ba9f858168def75eb1b63051b3cb9fa6
BerkiManman <berkayguler7@hotmail.com>
2023-03-21 18:23:57 +0300

    Admin | Added admin dashboard with utilities including deleting users and categories

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,12 +1,28 @@
 const Category = require('../models/Category');
+const Course = require('../models/Course');
 
 exports.createCategory = async (req, res) => {
     try {
         const category = await Category.create(req.body);
-        res.status(201).json({
-            status: 'success',
-            category,
+        res.status(201).redirect('/users/dashboard');
+    } catch (error) {
+        res.status(400).json({
+            error,
         });
+    }
+};
+
+exports.deleteCategory = async (req, res) => {
+    try {
+        const courses = await Course.find({ category: req.params.id });
+        if (courses.length > 0) {
+            await Course.updateMany(
+                { category: req.params.id },
+                { category: '64199669349394bb896893be' } //hardcoded id of uncategorized
+            );
+        }
+        await Category.findByIdAndRemove(req.params.id)
+        res.status(200).redirect('/users/dashboard');
     } catch (error) {
         res.status(400).json({
             error,
--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -3,8 +3,8 @@ const categoryController = require('../controllers/categoryController');
 
 const router = express.Router();
 
-router.route('/')
-    .post(categoryController.createCategory);
+router.post('/', categoryController.createCategory);
+router.delete('/:id', categoryController.deleteCategory);
 
 // router.route('/:slug')
 //     .get(categoryController.getCategory)
